19cb4d211e65839392001e1428d80d6884110a75
Tim Escher <tim@eschers.com>
2017-02-04 08:11:12 -0900

    Debugging

--- runners/sendQueued.js
+++ runners/sendQueued.js
@@ -1,9 +1,9 @@
 var runnerScript = require("../sendQueued.js");
 
 runnerScript().then(function(success) {
-  console.log("Success: " + success.toString);
+  console.log("Success: " + success.toString());
   process.exit(0);
 }, function(err) {
-  console.log("Error: " + err.toString);
+  console.log("Error: " + err.toString());
   process.exit(1);
 });
