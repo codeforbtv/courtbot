5d34a212498461ab4bfaf657694ff4744b31f593
Tim Escher <tim@eschers.com>
2017-02-15 19:28:38 -0900

    debugginf

--- runners/sendReminders.js
+++ runners/sendReminders.js
@@ -4,6 +4,6 @@ runnerScript().then(function(success) {
   console.log("Success: ",success);
   process.exit(0);
 }, function(err) {
-  console.log("Error: ",err);
+  console.log("Error: " + err.toString());
   process.exit(1);
 });
